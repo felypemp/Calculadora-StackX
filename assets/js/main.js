bbfb4456de55b2d6c845fb00bcb56de13facbda3
Felipe <fmp.ti@outlook.com>
2023-08-22 11:10:15 -0300

    :bug: Corrigindo bug na função para capturar a data e hora atual

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,6 @@
-const tabelaHistorico = document.querySelector('.Table-Historico');
-const data = new Date();
-
-
-////////////////////////////////////////////////////////////////////////
 
 function adicionarHistorico(data, resultado){
+  const tabelaHistorico = document.querySelector('.Table-Historico');
   const tr = document.createElement('tr');
   const td1 = document.createElement('td');
   const td2 = document.createElement('td');
@@ -26,16 +22,17 @@ function zeroEsquerda(num){
 
 }
 
-function formataData(data){
-  const dia = zeroEsquerda(data.getDate());
-  const mes= zeroEsquerda(data.getMonth() +1);
-  const ano= data.getFullYear();
-  const horas= zeroEsquerda(data.getHours());
-  const minutos= zeroEsquerda(data.getMinutes());
-  const segundos= zeroEsquerda(data.getSeconds());
-
-  return `${dia}/${mes}/${ano} ${horas}:${minutos}:${segundos}`
-}
+const dataAtual = () => {
+    const data = new Date();
+    const dia = zeroEsquerda(data.getDate());
+    const mes= zeroEsquerda(data.getMonth() +1);
+    const ano= data.getFullYear();
+    const horas= zeroEsquerda(data.getHours());
+    const minutos= zeroEsquerda(data.getMinutes());
+    const segundos= zeroEsquerda(data.getSeconds());
+  
+    return `${dia}/${mes}/${ano} ${horas}:${minutos}:${segundos}`
+  }
 
 
 //////////////////////////////////////////////////////////////////////
@@ -67,7 +64,6 @@ function Calculadora(){
           return;
         }
 
-        var dataAtual = () => formataData(data)
         adicionarHistorico(dataAtual(), conta)
 
       }
@@ -105,6 +101,3 @@ function Calculadora(){
 
 const calculadora = new Calculadora;
 calculadora.inicia();
-
-
-/////////////////////////////////////////////////////////////////
